04b854102d011ba46f7a4132008a81316b14c226
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-03-14 13:50:07 +0100

    [TASK] Add composer file
    Resolves #75056
    
    Change-Id: Id5eae54e5321e5a503f7f0263002daf196c778c6
    Reviewed-on: https://review.typo3.org/47248
    Reviewed-by: Francois Suter <francois@typo3.org>
    Tested-by: Francois Suter <francois@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' =>
     array (
       'php' => '5.2.0-0.0.0',
-      'typo3' => '6.2.0-7.5.99',
+      'typo3' => '6.2.0-7.99.99',
       'expressions' => '',
     ),
     'conflicts' =>
